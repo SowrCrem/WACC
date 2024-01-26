e0aefbad1819d96099d13ffb949db1a303f10050
Prabhas Vedagiri <pv622@ic.ac.uk>
2024-01-26 12:44:45 +0000

    [pv622] Update invalidCompile.scala, Main.scala (Abstract compile function in main, Create sample tests for invalid wacc progs)

---
+++ test/invalidCompile.scala
@@ -0,0 +1,23 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+import wacc.Main
+
+class InvalidCompile extends AnyFlatSpec {
+
+  "invalid syntax test badComment.wacc" should "return exit code 100" in {
+    val path : Array[String] = Array("test/wacc/invalid/syntaxErr/basic/badComment.wacc")
+    val exitCode = Main.compile(path)
+    println("Exit Code: " + exitCode)
+    exitCode shouldBe 100
+  }
+
+
+
+  "invalid semantic test badComment.wacc" should "return exit code 200" in {
+    val path : Array[String] = Array("test/wacc/invalid/semanticErr/exit/badCharExit.wacc")
+    val exitCode = Main.compile(path)
+    println("Exit Code: " + exitCode)
+    exitCode shouldBe 200
+  }
+
+}

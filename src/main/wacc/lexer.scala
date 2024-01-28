1fd843c655ddbb922b7cf675cf002c9eb77f918c
at2222 <at2222@ic.ac.uk>
2024-01-28 17:39:26 +0000

    [at2222] Update lexerTests.scala - write literal tests

--- src/main/wacc/lexer.scala
+++ src/main/wacc/lexer.scala
@@ -1,16 +1,64 @@
 package wacc
-
 import parsley.Parsley
-import parsley.token.Lexer
+import parsley.token.{Lexer, predicate}
 import parsley.token.descriptions._
+import parsley.token.errors._
+import parsley.token.numeric._
+import parsley.token.symbol._
+import parsley.combinator._
+import parsley.character.{noneOf, string}
+
 
 object lexer {
-    private val desc = LexicalDesc.plain.copy(
-        // your configuration goes here
+  private val desc = LexicalDesc.plain.copy(
+    nameDesc = NameDesc.plain.copy(
+      identifierStart = predicate.Basic(_.isLetter),
+      identifierLetter = predicate.Basic(_.isLetterOrDigit)
+    ),
+    symbolDesc = SymbolDesc.plain.copy(
+      hardKeywords = Set(
+        "len",
+        "ord",
+        "chr",
+        "int",
+        "bool",
+        "char",
+        "string",
+        "pair",
+        "skip",
+        "read",
+        "free",
+        "return",
+        "exit",
+        "print",
+        "println",
+        "if",
+        "then",
+        "else",
+        "fi",
+        "while",
+        "do",
+        "done",
+        "begin",
+        "end",
+        "newpair",
+        "call",
+        "fst",
+        "snd",
+        "true",
+        "false",
+        "null"
+      )
     )
-    private val lexer = new Lexer(desc)
+  )
+  private val lexer = new Lexer(desc)
 
-    val integer = lexer.lexeme.integer.decimal
-    val implicits = lexer.lexeme.symbol.implicits
-    def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
+
+  val integer = lexer.lexeme.integer.decimal
+  val implicits = lexer.lexeme.symbol.implicits
+  val char: Parsley[Char] = lexer.lexeme.character.ascii
+  val string: Parsley[String] = lexer.lexeme.string.ascii
+  val ident: Parsley[String] = lexer.lexeme.names.identifier
+
+  def fully[A](p: Parsley[A]): Parsley[A] = lexer.fully(p)
 }
--- src/main/wacc/parser.scala
+++ src/main/wacc/parser.scala
@@ -1,20 +1,34 @@
 package wacc
-
 import parsley.{Parsley, Result}
 import parsley.expr.chain
 
+import parsley.errors.ErrorBuilder
+import parsley.combinator
 import lexer.implicits.implicitSymbol
-import lexer.{integer, fully}
+import lexer.{integer, fully, char, implicits, ident, string}
 
 object parser {
-    def parse(input: String): Result[String, BigInt] = parser.parse(input)
-    private val parser = fully(expr)
+    def parse(input: String): Result[String, Node] = parser.parse(input)
+    private val parser = fully(program)
+
+    private lazy val program: Parsley[Node] = expr
     
     private val add = (x: BigInt, y: BigInt) => x + y
     private val sub = (x: BigInt, y: BigInt) => x - y
 
-    private lazy val expr: Parsley[BigInt] =
-        chain.left1(integer | "(" ~> expr <~ ")")(
-            ("+" as add) | ("-" as sub)
-        )
+    private lazy val expr: Parsley[ExprNode] = atoms
+
+    lazy val atoms: Parsley[Atom] = {
+        val int = integer.map(n => {if (n.isValidInt) IntLiterNode(n.toInt) else ErrorNode("Integer too large")})
+        val bool = ("true" as BoolLiterNode(true)) | ("false" as BoolLiterNode(false))
+        val char = ("'" ~> lexer.char <~ "'") map CharLiterNode
+        val string = lexer.string map StringLiterNode
+        val ident = lexer.ident map IdentNode
+        val brackets = "(" ~> expr <~ ")" map BracketsNode
+        val pair = "null" as PairLiterNode()
+        
+        int | bool | char | string | ident | pair
+    }
+
+    
 }

1f049b7eca9ae6249a62448d1cbbd84f959e21ff
Hardiv Harshakumar <hh1222@ic.ac.uk>
2024-01-31 20:16:15 +0000

    [hh1222] Update parseAtoms.scala (Add parseFails() function to check for rejection)

--- test/unitTests/parseAtoms.scala
+++ test/unitTests/parseAtoms.scala
@@ -28,156 +28,152 @@ import wacc.Type
 
 class parseAtoms extends AnyFlatSpec {
 
-  def isSuccess[T](input: String, expected: T): Assertion = {
+  def parseFails(input: String, inputType: String): Assertion = {
+    parser.parse("begin " + inputType + " input = " + input + " end") should matchPattern { 
+      // Match on any Failure with any String
+      case Failure(_) => 
+    }
+  }
+
+  def parseSucceeds[T](input: String, expected: T): Assertion = {
     val identifier = Ident("input")
     var literalType = "int"
     var typeNode: Type = IntType()
     
     // Dynamically generate the literal node based on the expected type
     var literal: Node = Null()
-    var isError = false
     expected match {
       case exp: Int =>
-        literal = IntLiter(exp)
-        if (exp < 0) {
-          literal = Neg(IntLiter(exp * -1))
-        }
+        literal = if (exp < 0) Neg(IntLiter(exp * -1)) else IntLiter(exp)
       case exp: Boolean =>
         literalType = "bool"
-        literal = BoolLiter(exp)
-        typeNode = BoolType()
+        literal     = BoolLiter(exp)
+        typeNode    = BoolType()
       case exp: Char =>
         literalType = "char"
-        literal = CharLiter(exp)
-        typeNode = CharType()
+        literal     = CharLiter(exp)
+        typeNode    = CharType()
       case exp: String =>
         literalType = "string"
-        literal = StringLiter(exp)
-        typeNode = StringType()
+        literal     = StringLiter(exp)
+        typeNode    = StringType()
       case exp: Null =>
-        literalType = "int"
-        literal = Null()
-      case exp: Error =>
-        literal = Error("")
-        isError = true
+        literal     = Null()
+      case _ =>
+        throw new IllegalArgumentException("Unsupported type")
     }
 
-    if (isError) {
-      // Match on any Error with any String
-      return parser.parse("begin " + literalType + " input = " + input + " end") should matchPattern { 
-        case Error(_) => 
-      }
-    }
     val assignment = IdentAsgn(typeNode, identifier, literal)
     parser.parse("begin " + literalType + " input = " + input + " end") shouldBe Success(Program(List(), assignment))
   }
 
   // Tests for IntLiter -----------------------------------------------------------------------------------------------
   "the parser" should "parse unsigned int literals" in {
-    isSuccess("123", 123)
+    parseSucceeds("123", 123)
   }
 
   it should "parse signed positive int literals" in {
-    isSuccess("+123", 123)
+    parseSucceeds("+123", 123)
   }
 
   it should "parse signed negative int literals" in {
-    isSuccess("-123", -123)
+    parseSucceeds("-123", -123)
   }
 
   it should "reject doubly signed int literals" in {
     pending
-    isSuccess("--123", Error(""))
+    parseFails("++123", "int")
+    parseFails("--123", "int")
   }
 
   // Tests for BoolLiter ----------------------------------------------------------------------------------------------
   it should "parse true bool literals" in {
-    isSuccess("true", true)
+    parseSucceeds("true", true)
   }
 
   it should "parse false bool literals" in {
-    isSuccess("false", false)
+    parseSucceeds("false", false)
   }
 
   // Tests for CharLiter ----------------------------------------------------------------------------------------------
   it should "parse char literals" in {
-    isSuccess("\'a\'", 'a')
+    parseSucceeds("\'a\'", 'a')
   }
 
   it should "parse escaped char literals" in {
     pending
-    isSuccess("\'\\0\'", '\u0000')
-    isSuccess("\'\\b\'", '\b')
-    isSuccess("\'\\n\'", '\n')
-    isSuccess("\'\\f\'", '\f')
-    isSuccess("\'\\r\'", '\r')
-    isSuccess("\'\\t\'", '\t')
-    isSuccess("\'\\\'\'", '\'')
+    parseSucceeds("\'\\0\'" , '\u0000')
+    parseSucceeds("\'\\b\'" , '\b')
+    parseSucceeds("\'\\n\'" , '\n')
+    parseSucceeds("\'\\f\'" , '\f')
+    parseSucceeds("\'\\r\'" , '\r')
+    parseSucceeds("\'\\t\'" , '\t')
+    parseSucceeds("\'\\\'\'", '\'')
   }
 
   it should "reject invalid escaped char literals" in {
-    pending
-    isSuccess("\'\\a\'", Error(""))
-    isSuccess("\'\\z\'", Error(""))
-    isSuccess("\'\\\'", Error(""))
-    isSuccess("\'\\\"\'", Error(""))
-    isSuccess("\'\\x\'", Error(""))
-    isSuccess("\'\\u\'", Error(""))
-    isSuccess("\'\\U\'", Error(""))
+    // pending
+    parseFails("\'\\a\'" , "char")
+    parseFails("\'\\z\'" , "char")
+    parseFails("\'\\\'"  , "char")
+    parseFails("\'\\\"\'", "char")
+    parseFails("\'\\x\'" , "char")
+    parseFails("\'\\u\'" , "char")
+    parseFails("\'\\U\'" , "char")
   }
 
   it should "reject invalid char literals" in {
     pending
     // TODO: Should we include empty char? If not, write a test making sure its not rejected
-    isSuccess("\'\'"  , Error("")) // Empty char
-    isSuccess("\'\\\'", Error("")) // Backslash
-    isSuccess("\'\"\'", Error("")) // Double quote
-    isSuccess("\'\'\'", Error("")) // Single quote
+    parseFails("\'\'"  , "char") // Empty char
+    parseFails("\'\\\'", "char") // Backslash
+    parseFails("\'\"\'", "char") // Double quote
+    parseFails("\'\'\'", "char") // Single quote
   }
 
   // Tests for StringLiter --------------------------------------------------------------------------------------------
 
   it should "parse empty string literals" in {
-    isSuccess("\"\"", "")
+    parseSucceeds("\"\"", "")
   }
 
   it should "parse string literals" in {
-    isSuccess("\"hello\"", "hello")
-    isSuccess("\"hello world\"", "hello world")
+    parseSucceeds("\"hello\"", "hello")
+    parseSucceeds("\"hello world\"", "hello world")
   }
 
   it should "parse escaped string literals" in {
     pending
-    isSuccess("\"\\0\"", "\u0000")
-    isSuccess("\"\\b\"", "\b")
-    isSuccess("\"\\n\"", "\n")
-    isSuccess("\"\\f\"", "\f")
-    isSuccess("\"\\r\"", "\r")
-    isSuccess("\"\\t\"", "\t")
+    parseSucceeds("\"\\0\"", "\u0000")
+    parseSucceeds("\"\\b\"", "\b")
+    parseSucceeds("\"\\n\"", "\n")
+    parseSucceeds("\"\\f\"", "\f")
+    parseSucceeds("\"\\r\"", "\r")
+    parseSucceeds("\"\\t\"", "\t")
   }
 
   it should "reject invalid string literals" in {
     pending
-    isSuccess("\"\""  , Error("")) // Empty char
-    isSuccess("\"\\\"", Error("")) // Backslash
-    isSuccess("\"\"\"", Error("")) // Double quote
-    isSuccess("\"\'\"", Error("")) // Single quote
+    parseSucceeds("\"\""  , "string") // Empty char
+    parseSucceeds("\"\\\"", "string") // Backslash
+    parseSucceeds("\"\"\"", "string") // Double quote
+    parseSucceeds("\"\'\"", "string") // Single quote
   }
 
   it should "reject invalid escaped string literals" in {
     pending
-    isSuccess("\"\\a\"", Error(""))
-    isSuccess("\"\\z\"", Error(""))
-    isSuccess("\"\\\'\"", Error(""))
-    isSuccess("\"\\\"\"", Error(""))
-    isSuccess("\"\\x\"", Error(""))
-    isSuccess("\"\\u\"", Error(""))
-    isSuccess("\"\\U\"", Error(""))
+    parseSucceeds("\"\\a\"" , "string")
+    parseSucceeds("\"\\z\"" , "string")
+    parseSucceeds("\"\\\'\"", "string")
+    parseSucceeds("\"\\\"\"", "string")
+    parseSucceeds("\"\\x\"" , "string")
+    parseSucceeds("\"\\u\"" , "string")
+    parseSucceeds("\"\\U\"" , "string")
   }
 
   // Tests for Null (PairLiter) ---------------------------------------------------------------------------------------
   it should "parse null literals" in {
-    isSuccess("null", Null())
+    parseSucceeds("null", Null())
   }
 
   // Tests for Ident --------------------------------------------------------------------------------------------------

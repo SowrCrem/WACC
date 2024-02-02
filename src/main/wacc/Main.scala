e205e96368e86819e80fc6e49e85fad797cfa07b
Seth, Saurajit <saurajit.seth22@imperial.ac.uk>
2024-02-02 12:34:36 +0000

    [ss3722] Update Main (remove unnecessary print statements), add semanticAnalyser.scala (skeleton)

--- src/main/wacc/Main.scala
+++ src/main/wacc/Main.scala
@@ -5,9 +5,9 @@ import scala.sys.process._
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("hello WACC!")
-    args.foreach { println(_) }
-    println("Num Args= " + args.length)
+    // println("hello WACC!")
+    // args.foreach { println(_) }
+    // println("Num Args= " + args.length)
     val exitCode = compile(args)
     println("Exit Code: " + exitCode)
   }
@@ -15,10 +15,10 @@ object Main {
   def compile(args: Array[String]): Int = {
     args.headOption match {
       case Some(filename) => {
-        val expr = ("cat " + filename).!!
-        parser.parser.parse(expr) match {
-          case Success(x) => {
-            println(s"$expr = $x")
+        val fileContent = ("cat " + filename).!!
+        parser.parser.parse(fileContent) match {
+          case Success(node) => {
+            println(s"$fileContent = $node")
             0
           }
           case Failure(msg) => {

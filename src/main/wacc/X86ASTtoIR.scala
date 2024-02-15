97a46c63e7a264bca401f48c33a77d7f15d0ccc8
Prabhas Vedagiri <pv622@ic.ac.uk>
2024-02-15 18:32:15 +0000

    [pv622] Update X86ASTtoIR (fix recursive case for list of statements in astToIR)

--- src/main/wacc/X86ASTtoIR.scala
+++ src/main/wacc/X86ASTtoIR.scala
@@ -6,6 +6,11 @@ object X86IRGenerator {
 
   var exitFunc: Boolean = false;
 
+  /** 
+   * Generates the intermediate representation for the given AST
+   * @param ast - The AST of the program to be translated
+   * @return The intermediate representation of the program
+  */
   def generateIR(ast: Program): Buffer[Instruction] = {
     val instructions: Buffer[Instruction] = new ListBuffer[Instruction].empty
     instructions ++= List(
@@ -19,18 +24,25 @@ object X86IRGenerator {
     )
     instructions ++= astToIR(ast)
 
-    if (exitFunc) {
-      instructions ++= exitIR
-    }
-
+    
     instructions += Mov(REG(0), Immediate(0)) // Return 0 if no exit function
     instructions ++= List(
       PopRegisters(List(REG(1), FP)),
       ReturnInstr(),
-    )
-
+      )
+    if (exitFunc) {
+      instructions ++= exitIR
+    }
+    instructions
   }
 
+  /**
+    * Converts the given AST to intermediate representation
+    * by recursively traversing the AST and converting each node
+    * to a list of instructions in the IR.
+    * @param position
+    * @return The intermediate representation of the given AST
+    */
   def astToIR(position: Position): Buffer[Instruction] = position match {
     case Program(funcList, stat) => {
       val funcIR = new ListBuffer[Instruction]
@@ -38,18 +50,24 @@ object X86IRGenerator {
         funcIR.appendAll(astToIR(func))
       }
       val ir = new ListBuffer[Instruction]
+      val statInstrs = for (s <- stat) yield astToIR(s)
+      ir ++= statInstrs.flatten
       ir.appendAll(funcIR)
     }
     case Exit(IntLiter(value)) => {
       exitFunc = true;
+      println("reached")
       ListBuffer(
-        Mov(REG(0), Immediate(value.toInt)),
+        Mov(REG(0), Immediate(value)),
         Mov(REG(5), REG(0)),
         CallInstr("exit")
       )
     }
   }
 
+  /**
+    * The intermediate representation for the exit function
+    */
   val exitIR: List[Instruction] = List(
     Label("_exit"),
     Push(FP),

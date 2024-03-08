72de9ef59ad8d228521f8b52c0131dea650a50ca
at2222 <at2222@ic.ac.uk>
2024-03-08 12:00:56 +0000

    [at2222][pv622] Create WACCPreProcessor.scala (allow for C-style macro textual replacement)

---
+++ src/main/wacc/WACCPreProcessor.scala
@@ -0,0 +1,27 @@
+package wacc
+
+import scala.collection.mutable.HashMap
+import scala.util.matching.Regex
+
+object macroPreProcessor {
+  val macroMap = new HashMap[String, String]()
+
+  val definePattern: Regex = """~DEF (\w+) (.+)""".r
+  val macroUsagePattern: String => Regex = macroName => raw"\b$macroName\b".r
+
+  def preprocess(sourceCode: String): String = {
+    // Extract macro definitions from the source code
+    val macros = definePattern
+      .findAllMatchIn(sourceCode)
+      .foreach(m => macroMap.put(m.group(1), m.group(2)))
+
+       // Perform textual substitution for each macro definition
+    val x = macroMap.foldLeft(sourceCode) { case (code, (name, replacement)) =>
+      macroUsagePattern(name).replaceAllIn(code, replacement)
+    }
+
+    x
+  }
+
+  val testinputOne : String = "~DEF MACRO_1 = 5 \n begin println MACRO_1 end"
+}
